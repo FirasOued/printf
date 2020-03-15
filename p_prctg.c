c1cf487b701f354b8574940a76b8c631ac37f74c
FirasOued <oued.firas@hotmail.com>
2020-03-15 09:41:49 +0000

    print %  func

---
+++ p_prctg.c
@@ -0,0 +1,16 @@
+#include "holberton.c"
+
+/**
+*p_prctg - print porcentage character (%)
+*@n: va_list elemnts
+*@s: buffer (Temporary storage area)
+*@indx: buffer's position
+*Return: characters's number 
+*/
+
+int p_prctg(va_listrt n, char *s, int *indx)
+{
+	(void)n;
+	buffer(s, '%', indx);
+	return (1);
+}
